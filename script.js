d9d2179d3ff4b61b38fec19b4c32ac7fdb546c9a
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-02-22 16:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,65 @@
-function createPromise(count) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(count);
-    }, count * 1000);
+let table = document.querySelector("#output");
+ 
+function createRow(i, delay) {
+  // Create a new row
+  let newRow = table.insertRow();
+ 
+  // Insert two cells (columns) into the new row
+  let cell1 = newRow.insertCell(0);
+  let cell2 = newRow.insertCell(1);
+ 
+  // Add data to the cells
+  if (typeof i == "number") {
+    cell1.innerHTML = `Promise ${i}`;
+  } else {
+    cell1.innerHTML = i;
+  }
+  cell2.innerHTML = delay;
+}
+ 
+createRow("Loading...", "Loading...");
+ 
+function generateRandom() {
+  const randomNumber = (Math.random() * (3 - 1) + 1).toFixed(3);
+  return parseFloat(randomNumber);
+}
+ 
+const p1 = new Promise((resolve) => {
+  let delay = generateRandom();
+ 
+  setTimeout(() => {
+    resolve({ message: "P1 Passed", time: delay });
+  }, delay * 1000);
+});
+ 
+const p2 = new Promise((resolve) => {
+  let delay = generateRandom();
+ 
+  setTimeout(() => {
+    resolve({ message: "P2 Passed", time: delay });
+  }, delay * 1000);
+});
+ 
+const p3 = new Promise((resolve) => {
+  let delay = generateRandom();
+ 
+  setTimeout(() => {
+    resolve({ message: "P2 Passed", time: delay });
+  }, delay * 1000);
+});
+ 
+let promiseArr = [p1, p2, p3];
+ 
+Promise.all(promiseArr).then((values) => {
+  table.children[0].remove();
+  createRow(1, "");
+  createRow(2, "");
+  createRow(3, "");
+  let total = 0;
+  values.forEach((result, i) => {
+    total += result.time;
+    table.children[i].children[1].textContent = result.time;
   });
-}
-
-function addLoadingText() {
-  const output = document.getElementById("output");
-  output.innerHTML += `<tr id="loading"><td colspan="2">Loading...</td></tr>`;
-}
-
-function removeLoadingText() {
-  const output = document.getElementById("output");
-  output.innerHTML = "";
-}
-
-function updateTable(results) {
-  const output = document.getElementById("output");
-  results.forEach((time, index) => {
-    output.innerHTML += `<tr><td>Promise ${
-      index + 1
-    }</td><td>${time}</td></tr>`;
-  });
-
-  const totalTime = results.reduce((acc, curr) => acc + curr, 0);
-  output.innerHTML += `<tr><td>Total</td><td>${totalTime.toFixed(3)}</td></tr>`;
-}
-
-function main() {
-  const promises = [createPromise(1), createPromise(2), createPromise(3)];
-
-  addLoadingText();
-
-  Promise.all(promises)
-    .then((results) => {
-      removeLoadingText();
-      updateTable(results);
-    })
-    .catch((error) => {
-      console.error("Error occurred:", error);
-    });
-}
-
-window.onload = main;
+ 
+  createRow("Total", parseFloat(total.toFixed(3)));
+});
